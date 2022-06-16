d48b98a87cf7b2826abd2efb23e604581cc13049
Ebube Chinda <chindaebube03@gmail.com>
2022-06-16 23:39:30 +0100

    I added the functionalities to the app

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const question = [
+let isclicked
+var count = 0
+var score = 0
+var score1 = 0
+let question = [
     q1 = { 
         ques: "Full Meaning Of CSS",
         Answer : [
-            {correctAns:  "Cascading Style Sheet"},
+            {ans:  "Cascading Style Sheet"},
             {ans: "Cascading Sweet Shit"},
             {ans: "Cascading Sweet Style"}
         ]
@@ -11,7 +15,7 @@ const question = [
     q2 = { 
         ques: "What is Javascript Used for ",
         Answer : [
-            {correctAns: "Creating of dynamic and interactive web content"},
+            {ans: "Creating of dynamic and interactive web content"},
             {ans: "Functionalities"},
             {ans: "For Styling"}
         ]
@@ -19,35 +23,35 @@ const question = [
     q3 = { 
         ques: "What are methods in OOP",
         Answer : [
-            {correctAns: "They are used to perform certain actions"},
+            {ans: "They are used to perform certain actions"},
             {ans: "Storing Variables"},
             {ans: "I dont Know"}
         ]
     
     },
-    q = { 
+    q4 = { 
         ques: "Another name for methods",
         Answer : [
-            {correctAns: "Functions"},
+            {ans: "Functions"},
             {ans: "Variables"},
             {ans: "I dont Know"}
         ]
     
     },
-    q = { 
+    q5 = { 
         ques: "Full Meaning of OOP",
         Answer : [
-            {correctAns: "Object Oriented Programming"},
+            {ans: "Object Oriented Programming"},
             {ans: "Object Oriented Poop"},
             {ans: "I dont Know"}
         ]
        
     },
 
-    q = { 
+    q6 = { 
         ques: "What are variables",
           Answer : [
-            {correctAns: "A memory location"},
+            {ans: "A memory location"},
             {ans: "String Storage"},
             {ans: "I dont Know"}
         ]
@@ -60,16 +64,34 @@ const  container = document.querySelector('.question-container')
 const startBtn = document.querySelector('.start button')
 let nameValue = document.querySelector('.start input')
 const start = document.querySelector('.start')
-// const answers = document.querySelectorAll('.ans')
-const ans1 = document.querySelector('.ans1')
-const ans2 = document.querySelector('.ans2')
-const ans3 = document.querySelector('.ans3')
-const questions = document.querySelector('.question p')
+const answers = document.querySelectorAll('.ans')
+let questions = document.querySelector('.question p')
 const nextBtn = document.getElementById('next')
 const prevBtn = document.getElementById('prev')
 navigation.style.display = "none"
 options.style.display = "none"
 container.style.display = "none"
+const quiz =  () =>{
+    questions.textContent = question[count].ques
+   
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].textContent = question[count].Answer[i].ans
+        
+    }
+
+    for( const ans of answers){
+
+        ans.addEventListener('click',(e)=>{
+           if(e.target.textContent == question[count].Answer[0].ans ){
+            score+=10
+            console.log(score)
+           }        
+        })   
+  }
+
+}
+
+
 startBtn.addEventListener('click', () =>{
 
      if(nameValue.value == ""){
@@ -80,19 +102,39 @@ startBtn.addEventListener('click', () =>{
         navigation.style.display = "flex"
         options.style.display = "flex"
         container.style.display = "flex"
-         start.style.display = "none"
-         questions.textContent = question[0].ques
-         ans1.textContent = question[0].Answer[0].correctAns
-         ans2.textContent = question[0].Answer[1].ans
-         ans3.textContent = question[0].Answer[2].ans
-      
-
-         for (let index = 1; index < question.length; index++) {
-             
-             
-         }
-
-
-     }
+         start.style.display = "none" 
+         quiz()                 
+    }
 })
+
+
+ 
+nextBtn.addEventListener('click', () =>{
+    count+=1
+    if(count >= question.length){
+        console.log(`Your score is ${score}`)
+    }
+    else{
+            questions.textContent = question[count].ques
+            for ( i = 0; i < answers.length; i++) {
+            answers[i].textContent = question[count].Answer[i].ans
+            
+        }
+        for( const an of answers){
+
+            an.addEventListener('click',(e)=>{
+                if(e.target.textContent === question[count].Answer[0].ans ){
+                score+=10
+                    
+                }    
+                console.log(score)
+            
+            })        
+        } 
+
+    }
+         
+   })
+
+
 
